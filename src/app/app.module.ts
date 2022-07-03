4ef90675176595f8571af10579914c4def489bc7
Ricardo A <ricoutos@gmail.com>
2022-07-03 09:22:46 -0300

    Add reactive forms e conexão com api de contatos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
@@ -22,6 +24,8 @@ import { ContatoService } from "./contato.service";
     MatButtonModule,
     MatToolbarModule,
     MatInputModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule
   ],
   providers: [
--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ContatoService } from '../contato.service';
+import { Contato } from './contato';
+
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-contato',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+
+  constructor(
+    private service: ContatoService,
+    private fb: FormBuilder
+  ) {
+    this.formulario = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', Validators.email]
+    });
+  }
 
   ngOnInit(): void {
+  }
+  
+  submit() {
+    console.log(this.formulario.value);
+    // const c: Contato = new Contato();
+    // c.nome = 'Fulano';
+    // c.email = 'fulano@email.com';
+    // c.favorito = false;
+  
+    // this.service.save(c).subscribe( resposta => {
+    //   console.log(resposta)
+    // });
   }
 
 }
